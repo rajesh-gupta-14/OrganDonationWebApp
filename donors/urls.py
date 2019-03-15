b3cc27dfb4fffa958560e578ff50ab386bef59d1
Rajesh Kumar Gupta Lakshminarayan Gupta <gupta.rajesh1996@gmail.com>
2019-03-15 20:56:28 -0300

    Revert "User login and registration added"
    This reverts commit 79411cf21184eebc3637b2547284c4c5e188864c

--- donors/urls.py
+++ donors/urls.py
@@ -1,8 +1,7 @@
-from django.urls import re_path
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    re_path('register/$', views.donor_register, name='donor-register'),
-    re_path('login/$', views.donor_login, name='donor-login')
+    path('register/', views.register, name='donor-register'),
 ]
