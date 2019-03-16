bfd3b15ab2fb8f87321ac489f17eb6c0e29e27ce
rgupta <rajesh.gupta@dal.ca>
2019-03-16 18:18:29 -0300

    donor models registered with admin

--- donors/admin.py
+++ donors/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import DonationRequests, Appointments
 # Register your models here.
+admin.site.register(DonationRequests)
+admin.site.register(Appointments)
