6b52f2de7df00e34aa5d6868928eac910f0d0c6a
Rajesh Kumar Gupta Lakshminarayan Gupta <gupta.rajesh1996@gmail.com>
2019-03-15 21:22:51 -0300

    Revert "Revert "Revert "User login and registration added"""
    This reverts commit d661d8678aac6b6bf2e361c9593dddc6534aa36b

--- hospitals/views.py
+++ hospitals/views.py
@@ -1,44 +1,18 @@
-from django.shortcuts import render, redirect
-from .models import User
-from django.contrib.auth import login, logout, authenticate
-from django.views.decorators.csrf import csrf_protect
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
 # Create your views here.
-
-def hospital_register(request):
+def register(request):
     
     # If method is post
     if request.POST:
-        user = User()
-        user.username = request.POST.get("username","")
-        user.set_password(request.POST.get("password",""))
-        user.email = request.POST.get("email","")
-        user.first_name = request.POST.get("hospital_name","")
-        user.city = request.POST.get("city","")
-        user.province = request.POST.get("province","")
-        user.country = request.POST.get("country","")
-        user.contact_number = request.POST.get("contact_number","")
-        user.is_staff = True
-        user.save()
-        return redirect('hospital-login')
+        pass
     
     return render(request, "hospital-registration.html")
 
-def hospital_login(request):
+def login(request):
 
     # If method is post
     if request.POST:
-        username = request.POST.get("username", "")
-        password = request.POST.get("password", "")
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                if user.is_staff:
-                    login(request, user)
-                    if "next" in request.POST:
-                        return redirect(request.POST.get("next"))
-                    else:
-                        return render(request, "hospital-registration.html")
+        pass
 
     return render(request, "hospital-login.html")
