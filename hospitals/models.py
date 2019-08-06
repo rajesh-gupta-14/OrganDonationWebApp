f8d67bdeebfaed46160792a5aa7ed1da27bc6521
Rajesh Kumar Gupta Lakshminarayan Gupta <rj437434@dal.ca>
2019-08-06 16:39:49 -0300

    resolved

--- hospitals/models.py
+++ hospitals/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
 
+class User(AbstractUser):
+    hospital_name = models.CharField(max_length=200, null=True, blank=True)
+    province = models.CharField(max_length=100, null=True, blank=True)
+    contact_number = models.CharField(max_length=10, blank=True, null=True)
+    country = models.CharField(max_length=20, null=True)
+    city = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.username
 # Create your models here.
